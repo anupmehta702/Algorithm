7825c162a2224d371b66790a75bc4137c1e55f19
anupm <anupmehta702@gmail.com>
2019-08-27 14:22:39 +0530

    Adding tree code

--- src/com/algorithm/tree/BinaryTree.java
+++ src/com/algorithm/tree/BinaryTree.java
@@ -1,33 +1,247 @@
 package com.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTree {
-    Node root=null;
+    private Node root = null;
 
-    public void insertData(List<Integer> dataList){
-        for(int data : dataList){
-            insertNode(data);
+    public void insertData(List<Integer> dataList) {
+        for (int data : dataList) {
+            insertNode(root, data);
         }
     }
 
-    public Node insertNode(int data){
-        if(root == null){
-            return new Node(data,null,null);
+    public void insertData(int dataToAdd) {
+        System.out.println("Adding element - "+dataToAdd);
+        insertNode(root, dataToAdd);
+    }
+
+    private Node insertNode(Node current, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (current == null) { //these are leaf nodes that we are adding
+            return new Node(data);
+        }
+        if (current.data < data) {
+            current.right = insertNode(current.right, data);//forgot to do the assignment
+        } else if (current.data > data) {
+            current.left = insertNode(current.left, data);
+        } else {
+            return current;
+        }
+        return current;
+    }
+
+    public void deleteElement(int dataToDelete) {
+        System.out.println();
+        System.out.println("-- Tree structure before deletion of "+dataToDelete+" --");
+        print();
+        deleteElement(root, dataToDelete);
+        System.out.println("-- Tree structure after deletion of "+dataToDelete+" --");
+        print();
+        System.out.println();
+    }
+
+    public Node deleteElement(Node current, int dataToDelete) {
+        if (current == null) {
+            System.out.println("Root itself is null");
+            return null;
+        }
+        if (current.data > dataToDelete) {
+            current.left = deleteElement(current.left, dataToDelete);
+        } else if (current.data < dataToDelete) {
+            current.right = deleteElement(current.right, dataToDelete);
+        } else {
+            if (current.left != null && current.right != null) {
+                System.out.println("Data to delete is a node with both left and right children ");
+                //Find min from right subtree
+                Node minNodeFromRight = findMin(current.right);
+                //replace current with the min from right subtree
+                System.out.println("Replacing data to delete with -" + minNodeFromRight.data);
+                current.data = minNodeFromRight.data;
+                //delete the min Node from right (which would be always on the left of the current)
+                deleteElement(current.right, minNodeFromRight.data);//need to find the replaced root from right of current node
+
+            } else {
+                // if nodeToBeDeleted has only left child
+                if (current.left != null) {
+                    System.out.println("Data to delete had only left child .");
+                    current = current.left;
+                }// if nodeToBeDeleted has only right child
+                else if (current.right != null) {
+                    System.out.println("Data to delete had only right child .");
+                    current = current.right;
+                }// if nodeToBeDeleted has no child
+                else {
+                    System.out.println("Data to delete is a leaf ");
+                    current = null;
+                }
+            }
         }
 
-        return null;
+        return current;//by mistake returned null
     }
+
+    private Node findMin(Node root) {
+        if (root.left != null) {
+            root=findMin(root.left);//forgot to assign to root
+        }
+        return root;
+    }
+
+    public void print() {
+        LinkedList<Node> list = new LinkedList<>();
+        if (root == null) {
+            System.out.println("No nodes present");
+            return;
+        }
+        list.add(root);
+
+        while (!list.isEmpty()) {
+            Node temp = list.remove();
+            System.out.print(" " + temp.data);
+            if (temp.left != null) {
+                list.add(temp.left);
+            }
+            if (temp.right != null) {
+                list.add(temp.right);
+            }
+        }
+        System.out.println();
+    }
+
+    public int getDepth() {
+        int count =0;
+        LinkedList<Node> list = new LinkedList<>();
+        if (root == null) {
+            System.out.println("No nodes present");
+            return count;
+        }
+        list.add(root);
+        list.add(null);
+        count =1;
+
+        while (!list.isEmpty()) {
+            Node temp = list.remove();
+            System.out.print(" " + temp.data);
+
+            if (temp.left != null) {
+                list.add(temp.left);
+            }
+            if (temp.right != null) {
+                list.add(temp.right);
+            }
+        }
+        System.out.println();
+        return count;
+    }
+
+
+    public void containsNode(int dataToSearch) {
+        System.out.println(" value "+dataToSearch+" is present - "+containsNode(root, dataToSearch));
+    }
+
+    private boolean containsNode(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (current.data == value) {
+            return true;
+        }
+
+        return value > current.data ? containsNode(current.right, value) : containsNode(current.left, value);
+    }
+
+    public void printPath(int targetData){
+        List<Integer> path = new ArrayList<>();
+        printPath(root,path,targetData);
+        System.out.println("Printing path to "+targetData);
+        path.forEach((data)-> System.out.print(" -> "+data));
+        System.out.println();
+    }
+
+    public void printAllPath(){
+        printAllPath(root,new ArrayList<>());
+    }
+
+    private void printAllPath(Node current ,List<Integer> path ){
+        if(current == null){
+            return;
+        }else{
+            path.add(current.data);
+        }
+        if(current.right == null && current.left == null){
+            System.out.println("Printing path ");
+            path.forEach((data)-> System.out.print(" -> "+data));
+            System.out.println();
+        }else{
+            //below are the steps to traverse a node from left to right ie. print left first ,come to head,print right
+            printAllPath(current.left,path);
+            printAllPath(current.right,path);
+        }
+
+    }
+
+
+    private void printPath(Node current,List<Integer> path,int targetData){
+        if(targetData == current.data){
+            path.add(current.data);
+            return;
+        }else{
+            if(targetData > current.data){
+                path.add(current.data);
+                printPath(current.right,path,targetData);
+            }else{
+                path.add(current.data);
+                printPath(current.left,path,targetData);
+            }
+        }
+        return ;
+    }
+
+    public void doMirror(){
+        System.out.println("Tree before mirror");
+        print();
+        mirror(root);
+        System.out.println("Tree after mirror");
+        print();
+    }
+
+    private Node mirror(Node current){
+        if(current == null){
+            return current;
+        }
+        Node left = mirror(current.left);
+        Node right = mirror(current.right);
+        //THis is where you get a leaf node or a node ,you interchange thier left right
+        /*
+                     7
+                  4     8
+                 3 5      9
+         */
+        //Example first it goes to 3 ,then 5 ,then 4 swaps it's left right,then goes to 8 to then null,then 9,then null,
+        // then 8 again to swap it's right left,then goes to 7 and swaps 4 and 8
+        current.left = right;
+        current.right= left;
+
+        return current;
+    }
+
 
 }
+
 class Node {
     int data;
     Node right;
     Node left;
 
-    public Node(int data, Node right, Node left) {
+    public Node(int data) {
         this.data = data;
-        this.right = right;
-        this.left = left;
+        this.right = null;
+        this.left = null;
     }
 }
---
+++ src/com/algorithm/tree/BinaryTreeRunner.java
@@ -0,0 +1,115 @@
+package com.algorithm.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeRunner {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        List<Integer> inputList = new ArrayList<>();
+        inputList.add(6);inputList.add(4);inputList.add(8);inputList.add(3);inputList.add(5);inputList.add(7);inputList.add(9);
+        /*
+                     6
+                  4     8
+                 3 5   7 9
+         */
+
+        bt.insertData(inputList);
+        bt.print();
+
+        bt.containsNode(6);bt.containsNode(8);bt.containsNode(3);bt.containsNode(1);
+
+        bt.insertData(2);
+        bt.insertData(1);
+        /*
+                     6
+                  4     8
+                 3 5   7 9
+                2
+               1
+         */
+        bt.deleteElement(1);
+        bt.deleteElement(2);
+
+        bt.insertData(10);
+        bt.deleteElement(10);
+
+        bt.deleteElement(3);
+        bt.insertData(3);
+         /*
+                     6
+                  4     8
+                 3 5   7 9
+         */
+
+        bt.deleteElement(6);
+
+        bt.printPath(9);
+        bt.printPath(3);
+
+        bt.printAllPath();
+
+        bt.doMirror();
+
+    }
+
+}
+/*
+  6 4 8 3 5 7 9
+ value 6 is present - true
+ value 8 is present - true
+ value 3 is present - true
+ value 1 is present - false
+Adding element - 2
+Adding element - 1
+
+-- Tree structure before deletion of 1 --
+ 6 4 8 3 5 7 9 2 1
+Data to delete is a leaf
+-- Tree structure after deletion of 1 --
+ 6 4 8 3 5 7 9 2
+
+
+-- Tree structure before deletion of 2 --
+ 6 4 8 3 5 7 9 2
+Data to delete is a leaf
+-- Tree structure after deletion of 2 --
+ 6 4 8 3 5 7 9
+
+Adding element - 10
+
+-- Tree structure before deletion of 10 --
+ 6 4 8 3 5 7 9 10
+Data to delete is a leaf
+-- Tree structure after deletion of 10 --
+ 6 4 8 3 5 7 9
+
+
+-- Tree structure before deletion of 3 --
+ 6 4 8 3 5 7 9
+Data to delete is a leaf
+-- Tree structure after deletion of 3 --
+ 6 4 8 5 7 9
+
+Adding element - 3
+
+-- Tree structure before deletion of 6 --
+ 6 4 8 3 5 7 9
+Data to delete is a node with both left and right children
+Replacing data to delete with -7
+Data to delete is a leaf
+-- Tree structure after deletion of 6 --
+ 7 4 8 3 5 9
+
+Printing path to 9
+ -> 7 -> 8 -> 9
+Printing path to 3
+ -> 7 -> 4 -> 3
+Printing path
+ -> 7 -> 4 -> 3
+Printing path
+ -> 7 -> 4 -> 3 -> 5
+Printing path
+ -> 7 -> 4 -> 3 -> 5 -> 8 -> 9
+
+ */
