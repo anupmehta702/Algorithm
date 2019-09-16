43cd6970d8739ac026981f16fde32218403e31f3
anupm <anupmehta702@gmail.com>
2019-09-16 11:52:06 +0530

    Added test integer

--- src/com/java/concept/TestHashSet.java
+++ src/com/java/concept/TestHashSet.java
@@ -1,8 +1,108 @@
 package com.java.concept;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class TestHashSet {
 
-    public void crawl(){
+    public static void main(String[] args) {
+        testTreeSet();
+        testLinkedHashSet();
+    }
 
+    public static void testLinkedHashSet(){
+        Set<Order> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add(new Order(4,"D"));
+        linkedHashSet.add(new Order(1,"A"));
+        linkedHashSet.add(new Order(2,"B"));
+        linkedHashSet.add(new Order(3,"C"));
+        linkedHashSet.add(new Order(3,"C"));//TO avoid duplicates overrride equals and hashcode
+        System.out.print("LinkedHashSet -->");
+        for(Order input : linkedHashSet){
+            System.out.print(input);
+        }
+        System.out.println();
+    }
+
+
+
+    public static void testTreeSet(){
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(10);treeSet.add(20);
+        treeSet.add(5);treeSet.add(1);
+        System.out.print("TreeSet -->");
+        for(Integer input : treeSet){
+            System.out.print(" "+input);
+        }
+        System.out.println();
+        for(int index =0 ;index<treeSet.size();index++){
+            //treeSet has got no get function
+        }
+        System.out.print("Tree set using stream -->");
+        treeSet.stream().forEach((input)-> System.out.print(" "+input));
+        System.out.println();
+
+        /*
+        for(Integer input : treeSet){
+            System.out.print(" "+input);
+            treeSet.add(2);//Throws concurrentModification exception
+        }
+        */
+
+        Set<Order> treeSetForOrder = new TreeSet<>();
+        //No compile time exception ,but runtime exception if Order is not comparable
+        //java.lang.ClassCastException: com.java.concept.Order cannot be cast to java.lang.Comparable
+        treeSetForOrder.add(new Order(4,"D"));
+        treeSetForOrder.add(new Order(1,"A"));
+        treeSetForOrder.add(new Order(3,"C"));
+        treeSetForOrder.add(new Order(2,"B"));
+        treeSetForOrder.add(new Order(2,"B"));//uses compareTO to check for duplicate records
+        treeSetForOrder.add(new Order(6,"B"));
+        System.out.print("TreeSet<Order> -->");
+        for(Order inputOrder :treeSetForOrder){
+            System.out.print(" "+inputOrder);
+        }
+        System.out.println();
+    }
+}
+class Order implements Comparable<Order>{
+    int orderNumber=0;
+    String name;
+    public Order(int orderNumber, String name) {
+        this.orderNumber = orderNumber;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "orderNumber=" + orderNumber + ", name='" + name + '\'' + '}';
+    }
+
+    @Override
+    public int compareTo(Order o) {
+        //for ascending order
+        if(this.orderNumber < o.orderNumber)return -1;
+        if(this.orderNumber > o.orderNumber)return 1;
+        return 0;
+        /* For descending order
+        if(this.orderNumber < o.orderNumber)return 1;
+        if(this.orderNumber > o.orderNumber)return -1;
+        return 0;*/
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return orderNumber == order.orderNumber &&
+                Objects.equals(name, order.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderNumber, name);
     }
 }
--- src/com/java/concept/TestInteger.java
+++ src/com/java/concept/TestInteger.java
@@ -12,9 +12,24 @@ public class TestInteger {
         System.out.println("is Count2 == count3 " + (count2 == count3));//true
 
 
-        Integer count22 = Integer.valueOf(100);
-        Integer count33 = Integer.valueOf(100);
+        Integer count22 = Integer.valueOf(100);// Compiler converts this line to Integer a = Integer.valueOf(100);
+        Integer count33 = Integer.valueOf(100);// Compiler converts this line to Integer a = Integer.valueOf(100);
         System.out.println("is Count22 == count33 " + (count22 == count33));//true
+        /*
+        Integer.valueOf(i) returns cache value  if i lies between -127 to 128
+        code -
+         public static Integer valueOf(int i) {
+            if (i >= IntegerCache.low && i <= IntegerCache.high)
+                return IntegerCache.cache[i + (-IntegerCache.low)];
+            return new Integer(i);
+         }
+         */
+
+
+        Integer count222 = Integer.valueOf(128);
+        Integer count333 = Integer.valueOf(128);
+        System.out.println("is Count222 == count333 " + (count222 == count333));//false.Since value is greater than 127 ,it does not cache it
+
 
         Integer count4 = new Integer(4);
         System.out.println("is Count4 == ++count4 " + (count4 == (++count4)));//false
